c51b05e7bc98d1940fc49085ebdc55397feb9cd3
anuj taywade <anujtaywade456@gmail.com>
2025-09-06 17:39:23 +0530

    removed img option

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
 app.use("/auth", require("./routes/auth"));
